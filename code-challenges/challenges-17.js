8ad465d29024e136bae53c64fb185f005becba2a
Yousef Al Farwan <yousefsyria304@gmail.com>
2022-05-13 01:33:42 +0300

    exe 17 solved

---
+++ code-challenges/challenges-17.js
@@ -0,0 +1,137 @@
+'use strict';
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 01:
+// Required:
+//
+// referring to the given examples find out the pattern used and
+// Write a function that takes 2 integers and prints the patterns using recursion
+// 
+// Input: 16, 5
+// Output: [16, 11, 6, 1, -4, 1, 6, 11, 16]
+//
+// Input: 50, 9
+// Output: [50, 41, 32, 23, 14, 5, -4, 5, 14, 23, 32, 41, 50]
+//
+
+const recursionPattern = (int1, int2) => {
+   
+
+    let arr = []
+    
+    const instedFunction = (int1,int2)=>{
+        arr.push(int1)
+        if (int1<0) {
+            arr =arr.concat(arr.slice(0,arr.length-1).reverse())
+            return arr
+        }
+        else {
+            int1 = instedFunction(int1-int2,int2)
+            return arr
+        }
+
+    }
+
+    instedFunction(int1,int2)
+    return arr 
+    
+}
+
+
+
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 02:
+// Required:
+// 
+// Write a function that takes a string (HTML tag)
+// and extracts the link from the HTML tag
+//
+// Ex: <a href="http://www.hackerrank.com"><h1><b>HackerRank</b></h1></a> ==> "www.hackerrank.com"
+// Ex: <a href="http://www.something.org"><h1><b>HackerRank</b></h1></a> ==> "www.something.org"
+//
+// Note:
+//  Assume that links end with .com, .org or .net
+// 
+
+const filterLinks = (str) => {
+    // write your code here
+    let start = str.indexOf('www') 
+     let end 
+    if (str.includes('org')){
+          end = str.indexOf('org')-13
+    }
+
+    else if (str.includes('com')){
+          end = str.indexOf('com') -13
+    }
+
+     else if (str.includes('net')){
+          end = str.indexOf('net')-13
+    }
+    
+   return str.substr(start, end)
+
+
+
+}
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 03:
+// Required:
+// 
+// A phrase is considered palindrome if, after converting all uppercase letters into lowercase letters
+// and removing all non-alphanumeric characters, it reads the same forward and backward.
+// Alphanumeric characters include letters and numbers.
+// Given a string s, return true if it is a palindrome, or false otherwise.
+// 
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// as you can see "amanaplanacanalpanama" is a palindrome.
+//
+
+const isPalindrome = (str) => {
+    // write your code here
+    let rejectedLetter = /[\W_]/g  //remove the non-alphanumeric letters 
+
+    let getIntoLowerCase = str.toLowerCase().replace(rejectedLetter, '')  // converting the string to lower and replace the non-alphanumeric with ''
+
+    let getStrIntoReverse = getIntoLowerCase.split('').reverse().join('') // cutting into single letter and revers it back 
+
+    let checkBoolean = getStrIntoReverse === getIntoLowerCase // check if the string in lower case is the same string in reverse and return true of false 
+
+    return checkBoolean // return the final boolean value ( true || false)
+
+
+}
+
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 04:
+// Optional:
+// 
+//  Write a function that takes 2 arguments, one is a string and the other is an array
+//  the function should return a boolean than indicates if both have the same pattern
+//
+//  EX: 
+//  Input: "abba", ['cat', 'dog', 'dog', 'cat']
+//  output: true
+//
+//  as you can see the a acted as the cat in the array, and b as the dog, and the pattern was the same
+//
+//  Input: "cda", ['cat', 'dog', 'cat']
+//  output: false
+//
+//  here if the pattern had three different things so the array should had 3 different things to be true
+//
+
+const samePattern = (str, arr) => {
+    // write your code here
+}
+// -------------------------------------------------------------------------------------------------------
+
+
+module.exports = { recursionPattern, filterLinks, isPalindrome, samePattern };
